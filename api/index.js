5898f11bc7f4d96f10f947eab9eafd6429d4ad16
metejada <metejada@bgeneral.com>
2022-06-12 13:32:37 -0500

    se agrega funcion para authprizar un accessToken

--- api/index.js
+++ api/index.js
@@ -14,6 +14,16 @@ app.post("/api/auth/token", function(req, res) {
   res.json({ access_token: token });
 });
 
+app.get("/api/auth/token/authorizer", function(request, response, next){
+const {access_token} = request.query;
+    try{
+            const decoded = jwt.verify(access_token, config.authJwtSecret);
+            response.json({ message: "The access Token is valid", username: decoded.sub })
+        }catch(err){
+            next(err);
+        }
+});
+
 const server = app.listen(5000, function() {
   console.log(`Listening http://localhost:${server.address().port}`);
 });
